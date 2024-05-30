a0cebca294b48f82b93b1cba496d7816d84e50c9
Akanni Ajibola <jilboy72@gmail.com>
2024-05-30 11:24:29 +0100

    Added new feature

--- lib/widget/setting.dart
+++ lib/widget/setting.dart
@@ -11,19 +11,99 @@ class Setting extends StatefulWidget {
 class _SettingState extends State<Setting> {
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Settings Page',
+          style: ThemeStles.primaryTitle,
+        ),
+        leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: const Icon(Icons.arrow_back)),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        child: ListView(
+          children: [
+            Row(
+              children: const [
+                Icon(Icons.person),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  'Account',
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            const Divider(
+              height: 20,
+              thickness: 1,
+            ),
+            buildAccountOption(context, 'Change Password'),
+            buildAccountOption(context, 'Context Settings'),
+            buildAccountOption(context, 'Social'),
+            buildAccountOption(context, 'Language'),
+            buildAccountOption(context, 'Privary and Security'),
+            const SizedBox(
+              height: 40,
+            ),
+            Row(
+              children: const [
+                Icon(
+                  Icons.volume_down,
+                  color: Colors.blue,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  'Notifications',
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  GestureDetector buildAccountOption(BuildContext context, String title) {
+    return GestureDetector(
+      onTap: () {
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text('title'),
+                content: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: const [Text('Option 1'), Text('Option 2')],
+                ),
+                actions: [
+                  TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('Close'))
+                ],
+              );
+            });
+      },
       child: Padding(
-        padding: const EdgeInsets.fromLTRB(150, 40, 50, 50),
+        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
         child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Text(
-              'Settings',
-              style: ThemeStles.primaryTitle,
+              title,
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
             ),
-            const SizedBox(
-              width: 20,
-            ),
-            const Icon(Icons.settings),
+            const Icon(Icons.arrow_forward_ios)
           ],
         ),
       ),
